49ee8260f80e371ea92b4fba8586afc9d768e20b
STALIN90 <stalint489@gmail.com>
2024-07-25 00:09:41 -0500

    primer commit

---
+++ src/main/java/com/springapp/Controlador/LibroControlador.java
@@ -0,0 +1,13 @@
+package com.springapp.Controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LibroControlador {
+@GetMapping("/libros")
+    public String Libros(){
+    return"libro.html";
+}
+
+}
